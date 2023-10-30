a04b849744db916fa6e2cdaeb9bdf67a4b94295a
Martin Helmich <m.helmich@mittwald.de>
2023-10-30 14:42:40 +0100

    Do not write into closed channel

--- api/mittwaldv2/poll.go
+++ api/mittwaldv2/poll.go
@@ -12,17 +12,26 @@ func poll[T any](ctx context.Context, f func() (T, error)) (T, error) {
 	res := make(chan T)
 	err := make(chan error)
 
-	defer close(res)
-	defer close(err)
-
 	t := time.NewTicker(200 * time.Millisecond)
 
+	defer func() {
+		t.Stop()
+		close(res)
+		close(err)
+	}()
+
 	go func() {
-		for range t.C {
+		for {
+			if _, ok := <-t.C; !ok {
+				return
+			}
+
 			r, e := f()
 			if e != nil {
 				if notFound := (ErrNotFound{}); errors.As(e, &notFound) {
 					continue
+				} else {
+					err <- e
 				}
 			} else {
 				res <- r
@@ -35,5 +44,7 @@ func poll[T any](ctx context.Context, f func() (T, error)) (T, error) {
 		return null, ctx.Err()
 	case r := <-res:
 		return r, nil
+	case e := <-err:
+		return null, e
 	}
 }
