2899bb19b8ff274f1056f19efe2bb3cfe59d0709
Martin Helmich <m.helmich@mittwald.de>
2024-01-05 11:37:10 +0100

    Fix panic when a nil *uuid.UUID is provided

--- internal/valueutil/string.go
+++ internal/valueutil/string.go
@@ -3,6 +3,7 @@ package valueutil
 import (
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"reflect"
 )
 
 func StringOrNull[T string](s T) types.String {
@@ -20,8 +21,24 @@ func StringPtrOrNull[T ~string](s *T) types.String {
 }
 
 func StringerOrNull(s fmt.Stringer) types.String {
-	if s == nil {
+	if isReallyNil(s) {
 		return types.StringNull()
 	}
 	return types.StringValue(s.String())
+}
+
+func isReallyNil(v any) bool {
+	if v == nil {
+		return true
+	}
+
+	rv := reflect.ValueOf(v)
+	switch rv.Kind() {
+	case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice:
+		return rv.IsNil()
+	case reflect.Array:
+		return rv.Len() == 0
+	default:
+		return false
+	}
 }
