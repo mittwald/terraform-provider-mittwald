bf17d01c90a5d7fac98ff00f0e7ff41dec251b23
Martin Helmich <m.helmich@mittwald.de>
2026-03-19 10:39:46 +0100

    refact(ai_api_key): readability

--- internal/provider/resource/aiapikeyresource/resource.go
+++ internal/provider/resource/aiapikeyresource/resource.go
@@ -120,7 +120,8 @@ func (r *Resource) ModifyPlan(ctx context.Context, req resource.ModifyPlanReques
 	}
 
 	customerIDIsNullOrUnknown := data.CustomerID.IsNull() || data.CustomerID.IsUnknown()
-	if customerIDIsNullOrUnknown && !data.ProjectID.IsNull() && !data.ProjectID.IsUnknown() {
+	projectIDIsNullOrUnknown := data.ProjectID.IsNull() || data.ProjectID.IsUnknown()
+	if customerIDIsNullOrUnknown && !projectIDIsNullOrUnknown {
 		project, err := r.getProject(ctx, data.ProjectID.ValueString())
 		if err != nil {
 			resp.Diagnostics.AddAttributeError(path.Root("project_id"), "Error retrieving project", fmt.Sprintf("Error retrieving project with ID %s: %s", data.ProjectID.ValueString(), err.Error()))
